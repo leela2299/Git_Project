1378db55c2eeeda2dca5753608c55b7dcb6f01ca
LEELA2299 <leelasuryatejam@gmail.com>
2023-01-16 10:55:28 -0800

    excpetions added

--- git_project.py
+++ git_project.py
@@ -1,4 +1,4 @@
-from git import Repo, InvalidGitRepositoryError, NoSuchPathError
+from git import Repo, InvalidGitRepositoryError, NoSuchPathError, CacheError
 import datetime
 import argparse
 import sys
@@ -15,7 +15,6 @@ class GitRepo:
             print("Invalid path to Git Directory")
             sys.exit(1)
 
-
     def get_path(self):
         parser = argparse.ArgumentParser(description ='Print Specific Facts about the Local Git Directory')
         parser.add_argument('--git_dir', type=str, required=True, metavar='', help='path of the local git directory')
@@ -23,27 +22,26 @@ class GitRepo:
         git_dir = args.git_dir
         return git_dir
         
-
     def get_active_branch(self):
         return self.repo.active_branch.name 
         
-    
     def is_modified(self):
-        # Returns true if the repository files are modified and uncommited
-        # This does not include untracked  files 
-        changed = [item.a_path for item in self.repo.index.diff('Head')]
-        print(changed)
-        return len(changed)>0
+        # Returns true if the modified repository files are uncommited
+        try:
+            staged = [item.a_path for item in self.repo.index.diff('Head')]
+            unstaged = [item.a_path for item in self.repo.index.diff(None)]
+            return len(staged)>0 or len(unstaged)>0
+        except CacheError:
+            print("Git Index error")
+            sys.exit(1)
 
     def is_head_commit_with_in_days(self, days = 7):
-
         curr_time = datetime.datetime.now()
         last_commit_time = (datetime.datetime.fromtimestamp(self.repo.head.object.authored_date))
         diff = (curr_time - last_commit_time).days
         return diff<=days
 
     def is_head_commit_by_user(self, user="Rufus"):
-        
         author = str(self.repo.head.object.author)
         return author == user
 
@@ -55,13 +53,6 @@ class GitRepo:
 
 
 
-
-
-
-
-repo = GitRepo()
-repo.display_details()
-# print("active branch : ",repo.get_active_branch())
-# print("local changes: ",repo.is_modified())
-# print("recent_commit : ",repo.is_head_commit_with_in_days())
-# print("blame Rufus : ",repo.is_head_commit_by_user())
+if __name__ == "__main__":
+    repo = GitRepo()
+    repo.display_details()
